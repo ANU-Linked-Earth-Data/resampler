c595d3858bcb4d55233d5eb8990133ab40ca0815
Dmitry Brizhinev <dmitry.brizhinev@anu.edu.au>
2016-05-20 01:03:07 +1000

    Resampler maked data arrays and a nice visualisation

---
+++ display.py
@@ -0,0 +1,35 @@
+import h5py
+import matplotlib.pyplot as plt
+import sys
+
+def cell_name(cell):
+    return '/'.join(str(cell))
+
+def show_tree(hdf5_name):
+    hdf5_file = h5py.File(hdf5_name, "r")
+    try:
+        hdf5_file.visit(lambda name: print(name))   
+    finally:
+        hdf5_file.close()
+    
+    
+def show_data(hdf5_name, cell):
+    hdf5_file = h5py.File(hdf5_name, "r")
+    try:
+        data = hdf5_file[cell_name(cell) + "/data"]
+        plt.imshow(data)
+        plt.show()
+    finally:
+        hdf5_file.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        show_tree(sys.argv[1])
+    elif len(sys.argv) == 3:
+        show_data(*sys.argv[1:])
+    else:
+        print("-------------------------------------------------------------")
+        print("Usage:")
+        print("display.py result.hdf5")
+        print("display.py result.hdf5 R7852")
+        print("-------------------------------------------------------------")
--- resampler.py
+++ resampler.py
@@ -9,11 +9,8 @@ import sys
 def cell_name(cell):
     return '/'.join(str(cell))
 
-def fromFile(filename, max_resolution, hdf5_file, band_num):
-    """ Given a GeoTIFF filename, returns an array of dicts (one for each band) of the form:
-        {cell : value}, for rHEALPix cells of the given resolution (we normally use 13, slightly larger than a landsat pixel)
-        each cell's value is interpolated from nearby pixels, and missing pixels are ignored
-    """
+def fromFile(filename, hdf5_file, band_num, max_resolution, resolution_gap):
+    """ Reads a geotiff file and converts the data into a hdf5 rhealpix file """
     dataset = gdal.Open(filename, gdalconst.GA_ReadOnly)
     width = dataset.RasterXSize
     height = dataset.RasterYSize
@@ -25,17 +22,13 @@ def fromFile(filename, max_resolution, hdf5_file, band_num):
         cells = rddgs.cells_from_region(resolution, (transform[0], transform[3]), (transform[0] + width * transform[1], transform[3] + height * transform[5]), plane=False)
         if len(cells) > 1:
             outer_res = resolution - 1
-            outer_cell = rddgs.cell_from_point(outer_res, (transform[0], transform[3]), plane=False)
             break
 
     print("Processing band ", band_num, "/", numBands, "...")
-    outer_group = hdf5_file.create_group(cell_name(outer_cell))
     band = dataset.GetRasterBand(band_num)
     missing_val = band.GetNoDataValue()
     masked_data = np.ma.masked_equal(band.ReadAsArray(), missing_val)
-    outer_pixel = outer_group.create_dataset('pixel', (1,))
-    outer_pixel[0] = masked_data.mean()
-    for resolution in range(outer_res + 1, max_resolution + 1):
+    for resolution in range(outer_res, max_resolution + 1):
         print("Processing resolution ", resolution, "/", max_resolution, "...")
         cells = rddgs.cells_from_region(resolution, (transform[0], transform[3]), (transform[0] + width * transform[1], transform[3] + height * transform[5]), plane=False)
         for cells2 in cells:
@@ -43,29 +36,52 @@ def fromFile(filename, max_resolution, hdf5_file, band_num):
                 north_west, _, south_east, _ = cell.vertices(plane=False)
                 left, top = north_west
                 right, bottom = south_east
-                left   = max(0, int((left   - transform[0]) / transform[1]))
-                right  = max(0, int((right  - transform[0]) / transform[1]))
-                top    = max(0, int((top    - transform[3]) / transform[5]))
-                bottom = max(0, int((bottom - transform[3]) / transform[5]))
-                value = masked_data[left:right,top:bottom].mean()
+                
+                left   = int((left   - transform[0]) / transform[1])
+                right  = int((right  - transform[0]) / transform[1])
+                top    = int((top    - transform[3]) / transform[5])
+                bottom = int((bottom - transform[3]) / transform[5])
+                
+                l = max(0, left)
+                r = max(0, right)
+                t = max(0, top)
+                b = max(0, bottom)
+                
+                value = masked_data[l:r+1,t:b+1].mean()
                 if value is not np.ma.masked:
                     group = hdf5_file.create_group(cell_name(cell))
                     pixel = group.create_dataset('pixel', (1,))
                     pixel[0] = value
+                    data = group.create_dataset('data', (3 ** resolution_gap, 3 ** resolution_gap))
+                    w = (right - left) / (3 ** resolution_gap)
+                    h = (bottom - top) / (3 ** resolution_gap)
+                    for x in range(3 ** resolution_gap):
+                        for y in range(3 ** resolution_gap):
+                            l = max(0, int(left+w*x))
+                            r = max(0, int(left+w*(x+1)))
+                            t = max(0, int(top+h*y))
+                            b = max(0, int(top+h*(y+1)))
+                            if l != r and t != b:
+                                value = masked_data[l:r+1,t:b+1].mean()
+                                if value is not np.ma.masked:
+                                    data[x,y] = value
+                                else:
+                                    data[x,y] = 0
+                             
 
-def run_code(tif_name, hdf5_name, max_res, band_num):
+def run_code(tif_name, hdf5_name, band_num, max_res, res_gap):
     hdf5_file = h5py.File(hdf5_name, "w")
     try:
-        fromFile(tif_name, max_res, hdf5_file, band_num)
+        fromFile(tif_name, hdf5_file, int(band_num), int(max_res), int(res_gap))
     finally:
         hdf5_file.close()
     print("Done!")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
+    if len(sys.argv) != 6:
         print("-------------------------------------------------------------")
-        print("Usage: resampler.py input.tif output.hdf5 max_resolution band")
+        print("Usage: resampler.py input.tif output.hdf5 band max_resolution resolution_gap")
         print("-------------------------------------------------------------")
-        print("Example: resampler.py test.tif result.hdf5 8 2")
+        print("Example: resampler.py test.tif result.hdf5 2 7 5")
     else:
-        run_code(sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
+        run_code(*sys.argv[1:])
