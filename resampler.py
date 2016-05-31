b1d9b1014e36f4ba06f0d0419d79af98c5603d89
Sam Toyer <sam@qxcv.net>
2016-05-31 21:47:32 +1000

    Metadata

--- resampler.py
+++ resampler.py
@@ -2,11 +2,22 @@
 
 from osgeo import gdal
 from osgeo import gdalconst
+import pytz
 import rhealpix_dggs.dggs as dggs
 import numpy as np
 import h5py
 from itertools import chain
+import re
 import sys
+
+
+# For parsing AGDC filenames
+AGDC_RE = re.compile(
+    r'^(?P<sat_id>[^_]+)_(?P<sensor_id>[^_]+)_(?P<prod_code>[^_]+)_'
+    r'(?P<lon>[^_]+)_(?P<lat>[^_]+)_(?P<year>\d{4})-(?P<month>\d{2})-'
+    r'(?P<day>\d{2})T(?P<hour>\d+)-(?P<minute>\d+)-(?P<second>\d+(\.\d+)?)'
+    r'\.tif$'
+)
 
 def cell_name(cell):
     return '/'.join(str(cell))
@@ -59,6 +70,58 @@ def make_cell_data(band_data, missing_value, resolution_gap, bottom, top, left, 
 
     return data
 
+
+def parse_agdc_fn(fn):
+    """Parses a filename in the format used by the AGDC Landsat archive. For
+    example, ``LS7_ETM_NBAR_149_-036_2012-02-10T23-50-47.650696.tif`` is a
+    Landsat 7 observation in GeoTIFF format taken on Februrary 10 2012 at
+    11:50pm (amongst other things).
+
+    >>> fn = 'LS7_ETM_NBAR_149_-036_2012-02-10T23-50-47.650696.tif'
+    >>> sorted_results = sorted(parse_agdc_fn(fn).items())
+    >>> print('\\n'.join('{}: {}'.format(k, v) for k, v in sorted_results))
+    datetime: 2012-02-10 23:50:47.650696+00:00
+    lat: -36.0
+    lon: 149.0
+    prod_code: NBAR
+    sat_id: LS7
+    sensor_id: ETM
+
+    :param string fn: Filename for observation.
+    :return: Dictionary of extracted metdata from filename.
+    """
+    match = AGDC_RE.match(fn)
+    if match is None:
+        raise ValueError('Invalid AGDC filename: "{}"'.format(fn))
+    info = match.groupdict()
+    raw_sec = float(info['second'])
+    int_sec = int(raw_sec)
+    microsecond = int(1e6 * (raw_sec % 1))
+    dt = pytz.datetime.datetime(
+        year=int(info['year']), month=int(info['month']), day=int(info['day']),
+        hour=int(info['hour']), minute=int(info['minute']),
+        second=int_sec, microsecond=microsecond, tzinfo=pytz.utc
+    )
+    return {
+        'lat': float(info['lat']), 'lon': float(info['lon']), 'datetime': dt,
+        'prod_code': info['prod_code'], 'sensor_id': info['sensor_id'],
+        'sat_id': info['sat_id']
+    }
+
+
+def add_meta(metadata, group):
+    """Add metadata, probably AGDC-related (e.g. acquisition time, satellite
+    ID), to an HDF5 group."""
+    for k, v in metadata.items():
+        if isinstance(v, pytz.datetime.datetime):
+            enc_v = v.isoformat()
+        else:
+            # Hope for the best!
+            enc_v = v
+        group.attrs[k] = enc_v
+    return group
+
+
 def fromFile(filename, hdf5_file, band_num, max_resolution, resolution_gap):
     """ Reads a geotiff file and converts the data into a hdf5 rhealpix file """
     dataset = gdal.Open(filename, gdalconst.GA_ReadOnly)
@@ -77,6 +140,8 @@ def fromFile(filename, hdf5_file, band_num, max_resolution, resolution_gap):
         if len(cells) > 1:
             outer_res = resolution - 1
             break
+
+    tif_meta = parse_agdc_fn(filename)
 
     print("Processing band ", band_num, "/", numBands, "...")
     band = dataset.GetRasterBand(band_num)
@@ -117,6 +182,7 @@ def fromFile(filename, hdf5_file, band_num, max_resolution, resolution_gap):
             # Write the HDF5 group. This is much faster than writing inline,
             # and lets us use numba.
             group = hdf5_file.create_group(cell_name(cell))
+            add_meta(tif_meta, group)
             group.attrs['bounds'] = np.array([
                 north_west, north_east, south_east, south_west, north_west
             ])
